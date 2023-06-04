f32f083ac93f99006e317f759ac05e9f0bf9671d
levu <you@example.com>
2023-06-04 21:15:35 +0700

    MahoaAES

---
+++ MahoaAES/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace MahoaAES
+{
+    partial class Form1
+    {
+        /// <summary>
+      
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+       
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtBM = new System.Windows.Forms.TextBox();
+            this.txtKhoaK = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnGiaiMa = new System.Windows.Forms.Button();
+            this.btnMaHoa = new System.Windows.Forms.Button();
+            this.txtBR = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.KQ = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(355, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Hoá AES";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Controls.Add(this.txtBM);
+            this.groupBox1.Controls.Add(this.txtKhoaK);
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.btnGiaiMa);
+            this.groupBox1.Controls.Add(this.btnMaHoa);
+            this.groupBox1.Controls.Add(this.txtBR);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(32, 69);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(834, 186);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(689, 107);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(110, 37);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // txtBM
+            // 
+            this.txtBM.Location = new System.Drawing.Point(118, 112);
+            this.txtBM.Multiline = true;
+            this.txtBM.Name = "txtBM";
+            this.txtBM.Size = new System.Drawing.Size(389, 46);
+            this.txtBM.TabIndex = 5;
+            // 
+            // txtKhoaK
+            // 
+            this.txtKhoaK.Location = new System.Drawing.Point(118, 76);
+            this.txtKhoaK.Name = "txtKhoaK";
+            this.txtKhoaK.Size = new System.Drawing.Size(389, 22);
+            this.txtKhoaK.TabIndex = 4;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(541, 107);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(110, 36);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Xoá";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnGiaiMa
+            // 
+            this.btnGiaiMa.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGiaiMa.Location = new System.Drawing.Point(689, 33);
+            this.btnGiaiMa.Name = "btnGiaiMa";
+            this.btnGiaiMa.Size = new System.Drawing.Size(110, 36);
+            this.btnGiaiMa.TabIndex = 5;
+            this.btnGiaiMa.Text = "Giải mã";
+            this.btnGiaiMa.UseVisualStyleBackColor = true;
+            // 
+            // btnMaHoa
+            // 
+            this.btnMaHoa.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaHoa.Location = new System.Drawing.Point(541, 33);
+            this.btnMaHoa.Name = "btnMaHoa";
+            this.btnMaHoa.Size = new System.Drawing.Size(110, 36);
+            this.btnMaHoa.TabIndex = 3;
+            this.btnMaHoa.Text = "Mã hoá";
+            this.btnMaHoa.UseVisualStyleBackColor = true;
+            this.btnMaHoa.Click += new System.EventHandler(this.btnMaHoa_Click);
+            // 
+            // txtBR
+            // 
+            this.txtBR.Location = new System.Drawing.Point(118, 31);
+            this.txtBR.Name = "txtBR";
+            this.txtBR.Size = new System.Drawing.Size(389, 22);
+            this.txtBR.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(32, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 23);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Bản mã";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 23);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Khoá K";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Bản Rõ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.KQ);
+            this.groupBox2.Location = new System.Drawing.Point(32, 262);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(834, 309);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            // 
+            // KQ
+            // 
+            this.KQ.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.KQ.Location = new System.Drawing.Point(3, 18);
+            this.KQ.Name = "KQ";
+            this.KQ.Size = new System.Drawing.Size(828, 288);
+            this.KQ.TabIndex = 0;
+            this.KQ.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 583);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtBM;
+        private System.Windows.Forms.TextBox txtKhoaK;
+        private System.Windows.Forms.TextBox txtBR;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnMaHoa;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnGiaiMa;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox KQ;
+    }
+}
+
---
+++ MahoaAES/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MahoaAES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public String[,] cypherText = new String[4, 4];
+        public String[,] plainText = new String[4, 4];
+        public String[,] khoa = new String[4, 4];
+        public String[,] khoa1 = new String[4, 4];
+        public String[,] khoa2 = new String[4, 4];
+        public String[,] khoa3 = new String[4, 4];
+        public String[,] khoa4 = new String[4, 4];
+        public String[,] khoa5 = new String[4, 4];
+        public String[,] khoa6 = new String[4, 4];
+        public String[,] khoa7 = new String[4, 4];
+        public String[,] khoa8 = new String[4, 4];
+        public String[,] khoa9 = new String[4, 4];
+        public String[,] khoa10 = new String[4, 4];
+        public String[,] addRoundKey = new String[4, 4];
+        public String[,] subBytes = new String[4, 4];
+        public String[,] shiftRows = new String[4, 4];
+        public String[,] mixColumns = new String[4, 4];
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtBR.Clear();
+            txtBM.Clear();
+            txtKhoaK.Clear();
+            KQ.Clear();
+            txtBR.Focus();
+        }
+
+        private void btnMaHoa_Click(object sender, EventArgs e)
+        {
+            KQ.Text = "";
+            input();
+            tao10khoa();
+            AddRoundKey();
+
+            for (int i = 1; i <= 9; i++)
+            {
+                KQ.Text += "Vòng lặp thứ " + i + " : " + "\n";
+                SubBytes();
+                ShiftRows();
+                MixColumns();
+                if (i == 1) AddRoundKeyvonglap(mixColumns, khoa1);
+                else if (i == 2) AddRoundKeyvonglap(mixColumns, khoa2);
+                else if (i == 3) AddRoundKeyvonglap(mixColumns, khoa3);
+                else if (i == 4) AddRoundKeyvonglap(mixColumns, khoa4);
+                else if (i == 5) AddRoundKeyvonglap(mixColumns, khoa5);
+                else if (i == 6) AddRoundKeyvonglap(mixColumns, khoa6);
+                else if (i == 7) AddRoundKeyvonglap(mixColumns, khoa7);
+                else if (i == 8) AddRoundKeyvonglap(mixColumns, khoa8);
+                else if (i == 9) AddRoundKeyvonglap(mixColumns, khoa9);
+
+            }
+            KQ.Text += "Bước tạo ngõ ra : " + "\n";
+            SubBytes();
+            ShiftRows();
+            KQ.Text += "Bản mã :\n";
+            for (int i = 0; i <= 3; i++)
+                for (int j = 0; j <= 3; j++)
+                {
+                    addRoundKey[i, j] = Aes.XOR16(shiftRows[i, j], khoa10[i, j]);
+                    if (j == 3) KQ.Text += "  " + addRoundKey[i, j] + "\n";
+                    else KQ.Text += "  " + addRoundKey[i, j] + " ";
+
+                }
+            for (int i = 0; i <= 3; i++)
+                for (int j = 0; j <= 3; j++)
+                {
+                    txtBM.Text += addRoundKey[i, j] + " ";
+                }
+        }
+        public void input()
+        {
+            String[] plainTextStr = txtBR.Text.Split(' ');
+            String[] key = txtKhoaK.Text.Split(' ');
+            int i = 0;
+            int j = 0;
+            foreach (String w in plainTextStr)
+            {
+                plainText[i, j] = w;
+                i++;
+                if (i > 3 && j != 3)
+                {
+                    i = 0;
+                    j++;
+                }
+
+            }
+            int a = 0;
+            int b = 0;
+            foreach (String w in key)
+            {
+                khoa[a, b] = w;
+                a++;
+                if (a > 3 && b != 3)
+                {
+                    a = 0;
+                    b++;
+                }
+
+            }
+
+        }
+        public void xuatmatran(String[,] matran)
+        {
+            for (int i = 0; i <= 3; i++)
+                for (int j = 0; j <= 3; j++)
+                {
+
+                    if (j == 3) KQ.Text += "  " + matran[i, j] + "\n";
+                    else KQ.Text += "  " + matran[i, j] + " ";
+
+                }
+        }
+        private void AddRoundKey()
+        {
+            KQ.Text += "AddRoundKey: \n";
+            for (int i = 0; i <= 3; i++)
+                for (int j = 0; j <= 3; j++)
+                {
+                    addRoundKey[i, j] = Aes.XOR16(plainText[i, j], khoa[i, j]);
+                    if (j == 3) KQ.Text += "  " + addRoundKey[i, j] + "\n";
+                    else KQ.Text += "  " + addRoundKey[i, j] + " ";
+                }
+        }
+        private void AddRoundKeyvonglap(String[,] kqdr, String[,] khoamoi)
+        {
+            KQ.Text += "AddRoundKey (input đầu ra của vòng lặp kết hợp với Khóa mới): \n";
+            for (int i = 0; i <= 3; i++)
+                for (int j = 0; j <= 3; j++)
+                {
+                    addRoundKey[i, j] = Aes.XOR16(kqdr[i, j], khoamoi[i, j]);
+                    if (j == 3) KQ.Text += "  " + addRoundKey[i, j] + "\n";
+                    else KQ.Text += "  " + addRoundKey[i, j] + " ";
+
+                }
+        }
+        private void SubBytes()
+        {
+            KQ.Text += "SubBytes : \n";
+            for (int i = 0; i <= 3; i++)
+                for (int j = 0; j <= 3; j++)
+                {
+                    String ark = addRoundKey[i, j];
+                    subBytes[i, j] = Aes.sbox[Aes.chuyen16sangso(ark[0]), Aes.chuyen16sangso(ark[1])];
+                    if (j == 3) KQ.Text += "  " + subBytes[i, j] + "\n";
+                    else KQ.Text += "  " + subBytes[i, j] + " ";
+                }
+        }
+        public void ShiftRows()
+        {
+            String teap;
+            teap = subBytes[1, 0];
+            subBytes[1, 0] = subBytes[1, 3];
+            subBytes[1, 3] = teap;
+            teap = subBytes[1, 0];
+            subBytes[1, 0] = subBytes[1, 2];
+            subBytes[1, 2] = teap;
+            teap = subBytes[1, 0];
+            subBytes[1, 0] = subBytes[1, 1];
+            subBytes[1, 1] = teap;
+
+            teap = subBytes[2, 0];
+            subBytes[2, 0] = subBytes[2, 2];
+            subBytes[2, 2] = teap;
+            teap = subBytes[2, 1];
+            subBytes[2, 1] = subBytes[2, 3];
+            subBytes[2, 3] = teap;
+
+            teap = subBytes[3, 0];
+            subBytes[3, 0] = subBytes[3, 3];
+            subBytes[3, 3] = teap;
+            teap = subBytes[3, 1];
+            subBytes[3, 1] = subBytes[3, 3];
+            subBytes[3, 3] = teap;
+            teap = subBytes[3, 2];
+            subBytes[3, 2] = subBytes[3, 3];
+            subBytes[3, 3] = teap;
+
+            shiftRows = subBytes;
+            KQ.Text += " ShiftRows : \n";
+            xuatmatran(shiftRows);
+        }
+        public void MixColumns()
+        {
+            for (int j = 0; j <= 3; j++)
+            {
+                String[] lay1cot = new String[4];
+                for (int i = 0; i <= 3; i++)
+                {
+                    lay1cot[i] = shiftRows[i, j];
+                }
+                for (int k = 0; k <= 3; k++)
+                {
+                    String[] kqsaubangbd = new String[4];
+                    for (int q = 0; q <= 3; q++)
+                    {
+                        kqsaubangbd[q] = Aes.nhanbangbd(lay1cot[q], Aes.matranbd[k, q]);
+                    }
+                    mixColumns[k, j] = Aes.XOR16voi4kytu(kqsaubangbd[0], kqsaubangbd[1], kqsaubangbd[2], kqsaubangbd[3]);
+                }
+            }
+            KQ.Text += " MixColumns: \n";
+            xuatmatran(mixColumns);
+        }
+        public void tao10khoa()
+        {
+            KQ.Text += "Tính khóa : " + "\n";
+            for (int i = 1; i <= 10; i++)
+            {
+                String[] R_con = new String[4];
+                R_con = Aes.layRcon(i);
+                if (i == 1)
+                {
+                    khoa1 = Aes.tinhkhoa(R_con, khoa);
+                    KQ.Text += "Khóa 1 " + "\n";
+                    xuatmatran(khoa1);
+                }
+                else if (i == 2)
+                {
+                    khoa2 = Aes.tinhkhoa(R_con, khoa1);
+                    KQ.Text += "Khóa 2 " + "\n";
+                    xuatmatran(khoa2);
+                }
+                else if (i == 3)
+                {
+                    khoa3 = Aes.tinhkhoa(R_con, khoa2);
+                    KQ.Text += "Khóa 3 " + "\n";
+                    xuatmatran(khoa3);
+                }
+                else if (i == 4)
+                {
+                    khoa4 = Aes.tinhkhoa(R_con, khoa3);
+                    KQ.Text += "Khóa 4 " + "\n";
+                    xuatmatran(khoa4);
+                }
+                else if (i == 5)
+                {
+                    khoa5 = Aes.tinhkhoa(R_con, khoa4);
+                    KQ.Text += "Khóa 5 " + "\n";
+                    xuatmatran(khoa5);
+                }
+                else if (i == 6)
+                {
+                    khoa6 = Aes.tinhkhoa(R_con, khoa5);
+                    KQ.Text += "Khóa 6 " + "\n";
+                    xuatmatran(khoa6);
+                }
+                else if (i == 7)
+                {
+                    khoa7 = Aes.tinhkhoa(R_con, khoa6);
+                    KQ.Text += "Khóa 7 " + "\n";
+                    xuatmatran(khoa7);
+                }
+                else if (i == 8)
+                {
+                    khoa8 = Aes.tinhkhoa(R_con, khoa7);
+                    KQ.Text += "Khóa 8 " + "\n";
+                    xuatmatran(khoa8);
+                }
+                else if (i == 9)
+                {
+                    khoa9 = Aes.tinhkhoa(R_con, khoa8);
+                    KQ.Text += "Khóa 9 " + "\n";
+                    xuatmatran(khoa9);
+                }
+                else if (i == 10)
+                {
+                    khoa10 = Aes.tinhkhoa(R_con, khoa9);
+                    KQ.Text += "Khóa 10 " + "\n";
+                    xuatmatran(khoa10);
+                }
+            }
+        }
+    }
+}
